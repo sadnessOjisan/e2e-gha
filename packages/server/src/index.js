813a0a187fc18dc8e2baec44231572b9aa05de20
sadnessOjisan <sadness.ojisan@gmail.com>
2023-09-07 02:18:22 +0900

    init server

---
+++ packages/server/src/index.js
@@ -0,0 +1,24 @@
+// Import the framework and instantiate it
+import Fastify from "fastify";
+const fastify = Fastify({
+  logger: true,
+});
+
+// Declare a route
+fastify.get("/", async function handler(req, res) {
+  const q = req.query["q"];
+  res
+    .status(200)
+    .headers({
+      "content-type": "application/json",
+    })
+    .send({ q });
+});
+
+// Run the server!
+try {
+  await fastify.listen({ port: 3000 });
+} catch (err) {
+  fastify.log.error(err);
+  process.exit(1);
+}
